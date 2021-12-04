c7dd70cfd415c64d54a4ae188baaa31626c1ff38
Mary Joy Aban <abanmaryjoy0@gmail.com>
2021-12-04 15:22:50 +0800

    Play the Lottery

--- program1.py
+++ program1.py
@@ -2,27 +2,23 @@ import random
 
 def lotteryf():
     
-    tryagain= "y"
+    tryagain= "yes"
     while tryagain[0]=='y': 
         first= random.randint(0,9)
         second=random.randint(0,9)
         third=random.randint(0,9)
         randoms= [first, second, third]
-        
-        def numbinput():
-            numb1= int(input('\nEnter 1st number(0-9):    '))
-            numb2= int(input('Enter 2nd number(0-9):    '))
-            numb3= int(input('Enter 3rd number(0-9):    '))
-            return numb1, numb2,numb3
-        numbers= numbinput()
 
-                
-        if randoms==numbers :
+        numb1= int(input('\nEnter 1st number(0-9):    '))
+        numb2= int(input('Enter 2nd number(0-9):    '))
+        numb3= int(input('Enter 3rd number(0-9):    '))
+
+        if numb1 in randoms and numb2 in randoms and numb3 in randoms :
             print("\nYou Win! Congratulation")
             print(f"The winning numbers are  {randoms}.")
             tryagain= input("Want to play again? (y or n):    ")      
         else:
-            print ('\nYou loss')
+            print ('\nYou lose')
             print(f"The winning numbers are  {randoms}.")
             tryagain= input("Try Again? (y or n):    ")      
 
