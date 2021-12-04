48aa27bbc730bbc5d83caf70a423548a274fae93
Mary Joy Aban <abanmaryjoy0@gmail.com>
2021-12-04 14:45:08 +0800

    Guess the number

---
+++ program2.py
@@ -0,0 +1,17 @@
+print('    \nWelcome!!, Lets play a game: Guessing the Number')
+import random
+
+find= random.randint(0,100)
+
+guess= float(input('\nGuess the number between 0-100 :   '))
+while find != guess:
+    
+    if find>guess:
+        print(' Greater than')
+        guess= float(input('Guess again:   '))
+
+    elif find< guess:
+        print(' Less than')
+        guess= float(input('Guess again:   '))
+        
+print("     \nCongratulation! you Guess the number!")
